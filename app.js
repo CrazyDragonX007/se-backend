6d144c4d0d262e134ed7a81534c8cbfbbce6a26d
Chayan Khatry <ckhatrypro300@gmail.com>
2023-11-20 20:40:57 -0500

    fix login issue and bugs

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({credentials:true}));
-// app.options('*', cors());
+app.options('*', cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require("express-session")({
