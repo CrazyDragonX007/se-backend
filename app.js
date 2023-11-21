b4e3e52aea2dab9c10a2d70575b46718d61c001f
Chayan Khatry <ckhatrypro300@gmail.com>
2023-11-20 20:39:56 -0500

    fix login issue and bugs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({credentials:true}));
 // app.options('*', cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
